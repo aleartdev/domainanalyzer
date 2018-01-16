b6968b2c1ddb1b0b983d875cb982674fc6a1b558
freiholtz <freiholtzdavid@gmail.com>
2018-01-16 16:39:21 +0100

    Mindre fel mer rätt

--- domainanalyzer.py
+++ domainanalyzer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding=utf-8
 """Analyzes given domainnamne and pressents important information"""
 import sys
@@ -13,19 +13,31 @@ from dns import resolver
 # TODO: Highlight important information
 
 UNKNOWN = r'¯\_(ツ)_/¯'
+RESOVING_NAMESERVER = '8.8.8.8'
 
-
-
-# get domain from input arguments
+# Get domain name form arguments
 DOMAIN = sys.argv[1]
+
+# PROBLEM = sys.argv[2]
+# INFORMATION = information(DOMAIN)
+# SUGGESTIONS = analyze(INFORMATION, PROBLEM)
+# visualize(SUGGESTIONS)
 
 # strip extra domain information
 if '//' in DOMAIN:
     DOMAIN = DOMAIN.split("//")[-1].split("/")[0]
 
+# get punycode
+try:
+    DOMAIN.encode(encoding='utf-8').decode('ascii')
+except UnicodeDecodeError:
+    DOMAIN_PUNYCODE = DOMAIN.encode("idna").decode("utf-8")
+else:
+    DOMAIN_PUNYCODE = ''
+
+
 # resolve against google server to get more accurate whois
 RES = resolver.Resolver()
-RESOVING_NAMESERVER = '8.8.8.8'
 RES.nameservers = [RESOVING_NAMESERVER]
 
 
@@ -36,22 +48,27 @@ IPS = []
 if len(sys.argv) > 1:
 
     # get whois
+  
     WHOIS = pythonwhois.get_whois(DOMAIN, True)
 
     # get php version
-    RESULT = requests.get('http://{}'.format(DOMAIN))
+    PHP = UNKNOWN
     try:
-        PHP = RESULT.headers['X-Powered-By']
-    except KeyError:
-        PHP = UNKNOWN
+        RESULT = requests.get('http://{}'.format(DOMAIN))
+        try:
+            PHP = RESULT.headers['X-Powered-By']
+        except KeyError:
+            pass
+    except:
+        pass
 
     # calculate days left
     DAYSLEFT = (WHOIS['expiration_date'][0].date() - datetime.now().date()).days
-    EXP = '' if DAYSLEFT > 66 else 'Exp\t' + WHOIS['expiration_date'][0].strftime("%Y-%m-%d") + ' (' + str(DAYSLEFT) + ' days left)'
+    EXP = '' if DAYSLEFT > 66 else 'EXP\t' + WHOIS['expiration_date'][0].strftime("%Y-%m-%d") + ' (' + str(DAYSLEFT) + ' days)'
 # calculate hours ago
     try:
         HOURSAGO = round((datetime.now().date() - WHOIS['updated_date'][0].date()).total_seconds() / 3600)
-        MOD = '' if HOURSAGO > 48 else 'Mod\t' + WHOIS['updated_date'][0].strftime("%Y-%m-%d") + " (%g hours ago)" % round(HOURSAGO, 0)
+        MOD = '' if HOURSAGO > 48 else 'Mod\t' + WHOIS['updated_date'][0].strftime("%Y-%m-%d") + " (%g hours)" % round(HOURSAGO, 0)
     except KeyError:
         MOD = 'Mod\tN/A'
 
@@ -83,22 +100,31 @@ if len(sys.argv) > 1:
         WHOIS_2 = pythonwhois.get_whois(IPS[0], True)
         try:
             print('ORG\t{}'.format(WHOIS_2['contacts']['registrant']['name']))
-        except KeyError:
+        except (KeyError, TypeError):
             try:
                 print('ORG\t{}'.format(WHOIS_2['emails'][0]))
             except KeyError:
                 print('ORG\t{}'.format(UNKNOWN))
 
     except dns.resolver.NXDOMAIN:
-        print('No such domain resolving {} against {}'.format(DOMAIN, RESOVING_NAMESERVER))
+        print('ERR\tNo such domain (NXDOMAIN)')
     except dns.resolver.Timeout:
-        print('Timed out while resolving {} against {}'.format(DOMAIN, RESOVING_NAMESERVER))
+        print('ERR\tTimeout')
     except dns.exception.DNSException:
-        print('Unhandled exception resolving {} against {}'.format(DOMAIN, RESOVING_NAMESERVER))
+        print('ERR\tDNSException')
 
-    print('MX\t{}'.format(subprocess.check_output(['dig', '+noall', '+answer', 'MX', DOMAIN]).strip()))
-    print('TXT\t{}'.format(subprocess.check_output(['dig', '+noall', '+answer', 'TXT', DOMAIN]).strip()))
+    MX = subprocess.check_output(['dig', '+noall', '+answer', 'MX', DOMAIN]).decode('unicode_escape').strip().replace('\n','\n\t')
+    if MX:
+        print('MX\t{}'.format(MX))
+    else:
+        print('MX\t{}'.format(UNKNOWN))
 
+
+    print('TXT\t{}'.format(subprocess.check_output(['dig', '+noall', '+answer', 'TXT', DOMAIN]).decode('unicode_escape').strip()))
+
+    if DOMAIN_PUNYCODE:
+        print('PUNY\t{}'.format(DOMAIN_PUNYCODE))
+    
     # if you want to open domain in browser
     # webbrowser.open('http://' + DOMAIN)
 
